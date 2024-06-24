6e96e7a9d100b4fa7de59f1c3dfb5bfb3ed81381
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-24 17:36:17 +0100

    Ensure grid will always be a square

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector(".grid");
+const GRIDWIDTH = 1024;
+grid.style.width = `${GRIDWIDTH}px`;
+
 const button = document.querySelector(".button");
 
 function isValidGridSize(size) {
     // check the user input is between 1 and 100 (exclusive)
-    if (size < 1 || size > 99) {
-        alert("Please enter a value between 1 and 100");
+    if (size < 1 || size > 20) {
+        alert("Please enter a value between 1 and 20");
     }
 
     return true;
@@ -22,6 +25,10 @@ function createGrid(size) {
     for (let i = 1; i <= gridSize; i++) {
         const square = document.createElement("div");
         square.setAttribute("class", `square square-${i}`);
+        
+        // set the width of the grid
+        square.style.width = `${GRIDWIDTH / size}px`;
+
         // add an event listener to change the div background on mouse hover
         square.addEventListener("mouseover", () => {
             square.classList.add("draw");
@@ -35,7 +42,7 @@ button.addEventListener("click", () => {
     clearGrid();
 
     // get the size of the new grid to be created
-    let userInput = prompt("Enter size of new grid between 1 - 100");
+    let userInput = prompt("Enter size of new grid between 1 - 20");
 
     if (isValidGridSize(userInput)) {
         createGrid(userInput);
