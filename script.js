090e1c96e96d8b2ec5f2692c5dba6a43ca00d3f0
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-24 16:42:16 +0100

    Add ability to draw over grid

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ grid = document.querySelector(".grid");
 for (let i = 1; i <= 256; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", `square square-${i}`);
+    // add an event listener to change the div background on mouse hover
+    square.addEventListener("mouseover", () => {
+        square.classList.add("draw");
+    });
     grid.appendChild(square);
 }
 
